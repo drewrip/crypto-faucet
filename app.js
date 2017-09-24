1577b792c6233b3b578c05bed0a5b9b8f28257d9
Drew Ripberger <drew.ripberger@gmail.com>
2017-09-23 22:45:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 // Retrieve
 var fs = require("fs");
 var http = require("http");
-var coind = require("node-coind");
+var walletRPC = require("bitcoin-core");
 
-var client = new coind.Client({
-	host: "localhost",
-	port: ,
-	user: "",
-	pass: ""
+const walletSIGT = new walletRPC({
+  host: "localhost",
+  port: 33334,
+  username: "",
+  password: ""
 });
 
-function newDeposit(fromAddr, amt){
-	db.addresses.insert({from: fromAddr, amount: amt})
-}
 http.createServer(function(request, response) {  
     response.writeHeader(200, {"Content-Type": "text/html"});
     response.write(fs.readFileSync("./index.html"));
-	$ = cheerio.load(html);
-	client.cmd('getbalance', '*', 6, function(err, balance){
+
+	walletSIGT.getBalance(function(err, balance){
 		if (err) return console.log(err);
 		console.log(balance);
-	});
-	client.cmd("getaddress", function(err, serverAddr){
-		if (err) return console.log(err);
-			console.log(serverAddr);
 	});
 
 	response.end();
